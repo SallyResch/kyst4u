0b3403035a7cccaabeeb273f42347fdfe6ed8a12
Sillysally <sallyresch27@gmail.com>
2023-05-02 15:36:49 +0200

    Made get method in frontend work to fetch all Users that is created

---
+++ src/pages/components/Admin.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+
+import Container from '@mui/material/Container';
+import { Paper } from '@mui/material';
+
+export default function Admin() {
+  const[users,setUsers]= useState([]);
+
+{/* To get all Users in a list from backend*/}
+useEffect (()=>{
+  fetch("http://localhost:8080/user/getAll")
+  .then(res=>res.json())
+  .then((result)=>{
+    setUsers(result);
+  })
+  .catch((error) =>{
+    console.log(error);
+  });
+},[]);
+  return (
+    <Container>
+    <h1>Check out all created users</h1>
+      <Paper elevation={3}>
+
+        {users.map(user=>(
+          <Paper elevation={6} key={user.id}>
+            Id:{user.id}
+            Email:{user.email}
+            Username: {user.username}
+            </Paper>
+        ))}
+      </Paper>
+    </Container>
+  )
+}
--- src/pages/components/User.jsx
+++ src/pages/components/User.jsx
@@ -1,14 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
-import { Button } from '@mui/material';
+import { Button,Paper } from '@mui/material';
 
 export default function User() {
 
-const [username, setUsername] = useState('');
 const [email, setEmail] = useState('');
+const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
+const [users,setUsers]=useState([]);
 
+{/* To post a user to backend */}
 const handleClick=(e)=>{
   e.preventDefault()
   const user={email,username,password}
@@ -18,28 +20,48 @@ const handleClick=(e)=>{
     headers:{"Content-Type":"application/json"},
     body: JSON.stringify(user)
   })
-  .then((response)=> {
-    if(!response.ok){throw new Error ('Network response was not ok');
-  }
+  .then(()=> {
     console.log("New User Created");
   })
-  .catch((error)=>{
-    console.error('There was a problem with the fetch operation', error);
-  });
 }
+
+{/* To get all Users in a list from backend*/}
+  useEffect (()=>{
+    fetch("http://localhost:8080/user/getAll")
+    .then(res=>res.json())
+    .then((result)=>{
+      setUsers(result);
+    })
+    .catch((error) =>{
+      console.log(error);
+    });
+  },[]);
+
+
   return (
       <Container>
         <h1>Create new User</h1>
+      <Paper>
       <form>
       <TextField id="outlined-basic" label="email" variant="outlined" value={email} onChange={(e)=>setEmail(e.target.value)}/>
       <TextField id="outlined-basic" label="username" variant="outlined" value={username} onChange={(e)=>setUsername(e.target.value)}/>
       <TextField id="outlined-basic" label="password" variant="outlined" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-      </form>
-  
+     
       <Button variant="contained" color='secondary' onClick={handleClick}>Create User</Button>
-      {email}
-      {username}
-      {password}
+      </form>
+      </Paper>
+
+      <h1>Check out all created users</h1>
+      <Paper elevation={3}>
+
+        {users.map(user=>(
+          <Paper elevation={6} key={user.id}>
+            Id:{user.id}
+            Email:{user.email}
+            Username: {user.username}
+            </Paper>
+        ))}
+      </Paper>
       </Container>
   );
 }

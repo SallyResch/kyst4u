4235b7e61ba154efb12f6f9616f467f4682fd6d6
Sillysally <sallyresch27@gmail.com>
2023-05-02 11:06:48 +0200

    added a button to be able to send a post request to backend

--- src/pages/components/User.jsx
+++ src/pages/components/User.jsx
@@ -1,13 +1,26 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
+import { Button } from '@mui/material';
 
 export default function User() {
+
+const [username, setUsername] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
   return (
       <Container>
-      <TextField id="outlined-basic" label="email" variant="outlined" />
-      <TextField id="outlined-basic" label="username" variant="outlined" />
-      <TextField id="outlined-basic" label="password" variant="outlined" />
+        <h1>Create new User</h1>
+      <form>
+      <TextField id="outlined-basic" label="email" variant="outlined" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <TextField id="outlined-basic" label="username" variant="outlined" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+      <TextField id="outlined-basic" label="password" variant="outlined" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      </form>
+      {email}
+      {username}
+      {password}
+
+      <Button variant="contained" color='secondary'>Create User</Button>
       </Container>
   );
 }

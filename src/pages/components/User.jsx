d574d5b50eb3bc0d3f249ce85a340d33b1f7a7c4
Sillysally <sallyresch27@gmail.com>
2023-05-02 11:40:52 +0200

    got save user to work in frontend to connect to backend

--- src/pages/components/User.jsx
+++ src/pages/components/User.jsx
@@ -8,6 +8,25 @@ export default function User() {
 const [username, setUsername] = useState('');
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
+
+const handleClick=(e)=>{
+  e.preventDefault()
+  const user={email,username,password}
+  console.log(user)
+  fetch("http://localhost:8080/user/save",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify(user)
+  })
+  .then((response)=> {
+    if(!response.ok){throw new Error ('Network response was not ok');
+  }
+    console.log("New User Created");
+  })
+  .catch((error)=>{
+    console.error('There was a problem with the fetch operation', error);
+  });
+}
   return (
       <Container>
         <h1>Create new User</h1>
@@ -16,11 +35,11 @@ const [password, setPassword] = useState('');
       <TextField id="outlined-basic" label="username" variant="outlined" value={username} onChange={(e)=>setUsername(e.target.value)}/>
       <TextField id="outlined-basic" label="password" variant="outlined" value={password} onChange={(e)=>setPassword(e.target.value)}/>
       </form>
+  
+      <Button variant="contained" color='secondary' onClick={handleClick}>Create User</Button>
       {email}
       {username}
       {password}
-
-      <Button variant="contained" color='secondary'>Create User</Button>
       </Container>
   );
 }

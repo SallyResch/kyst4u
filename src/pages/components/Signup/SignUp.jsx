91ad9d80519cce0a602daa991c6777676727ce6c
Sillysally <sallyresch27@gmail.com>
2023-05-09 09:16:07 +0200

    made POST of register user work from front- to backend

--- src/pages/components/Signup/SignUp.jsx
+++ src/pages/components/Signup/SignUp.jsx
@@ -13,7 +13,7 @@ const [password, setPassword] = useState('');
 {/* To post a user to backend */}
 const handleClick = (e) => {
   e.preventDefault();
-  const user = { email, username, password };
+  const user = { firstname, lastname, email, password };
   console.log(user);
   const token = localStorage.getItem('token'); // Retrieve token from localStorage
   fetch('http://localhost:8080/api/v1/auth/register', {
@@ -38,8 +38,8 @@ const handleClick = (e) => {
       <Paper>
       <form>
       
-      <TextField id="outlined-basic" label="firstname" variant="outlined" value={email} onChange={(e)=>setFirstname(e.target.value)}/>
-      <TextField id="outlined-basic" label="lastname" variant="outlined" value={email} onChange={(e)=>setLastname(e.target.value)}/>
+      <TextField id="outlined-basic" label="firstname" variant="outlined" value={firstname} onChange={(e)=>setFirstname(e.target.value)}/>
+      <TextField id="outlined-basic" label="lastname" variant="outlined" value={lastname} onChange={(e)=>setLastname(e.target.value)}/>
       <TextField id="outlined-basic" label="email" variant="outlined" value={email} onChange={(e)=>setEmail(e.target.value)}/>
       <TextField id="outlined-basic" label="password" variant="outlined" value={password} onChange={(e)=>setPassword(e.target.value)}/>
      

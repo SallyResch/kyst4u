a88ff68bdf9d3f5f5b7c93359c07d79b441a59c3
Sillysally <sallyresch27@gmail.com>
2023-05-09 08:59:11 +0200

    Made frontend fetch a jwt token instead

--- src/pages/components/Signup/SignUp.jsx
+++ src/pages/components/Signup/SignUp.jsx
@@ -5,35 +5,42 @@ import { Button,Paper } from '@mui/material';
 
 export default function SignUp() {
 
+const [firstname, setFirstname] = useState('');
+const [lastname, setLastname] = useState('');
 const [email, setEmail] = useState('');
-const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
 
 {/* To post a user to backend */}
-const handleClick=(e)=>{
-  e.preventDefault()
-  const user={email,username,password}
-  console.log(user)
-  fetch("http://localhost:8080/api/v1/auth/register",{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body: JSON.stringify(user)
+const handleClick = (e) => {
+  e.preventDefault();
+  const user = { email, username, password };
+  console.log(user);
+  const token = localStorage.getItem('token'); // Retrieve token from localStorage
+  fetch('http://localhost:8080/api/v1/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`, // Pass JWT token as Authorization header
+    },
+    body: JSON.stringify(user),
   })
-    .then(()=> {
-    console.log("New User Created");
+    .then(() => {
+      console.log('New User Created');
     })
-      .catch((error) => {
-      console.error("Error creating new user:", error);
-      });
-}     
+    .catch((error) => {
+      console.error('Error creating new user:', error);
+    });
+};    
 
   return (
       <Container>
         <h1>Create new User</h1>
       <Paper>
       <form>
+      
+      <TextField id="outlined-basic" label="firstname" variant="outlined" value={email} onChange={(e)=>setFirstname(e.target.value)}/>
+      <TextField id="outlined-basic" label="lastname" variant="outlined" value={email} onChange={(e)=>setLastname(e.target.value)}/>
       <TextField id="outlined-basic" label="email" variant="outlined" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      <TextField id="outlined-basic" label="username" variant="outlined" value={username} onChange={(e)=>setUsername(e.target.value)}/>
       <TextField id="outlined-basic" label="password" variant="outlined" value={password} onChange={(e)=>setPassword(e.target.value)}/>
      
       <Button variant="contained" color='secondary' onClick={handleClick}>Create User</Button>

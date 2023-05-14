964713257966059128fe3a321507f1a01473e08b
Sillysally <sallyresch27@gmail.com>
2023-05-14 15:21:37 +0200

    made the userPage not show if there is 0 token

--- src/pages/userPage.jsx
+++ src/pages/userPage.jsx
@@ -1,15 +1,58 @@
 import { useRouter } from "next/router";
 import { Button } from "@mui/material";
-import CheckUsers from "./components/AllUser/CheckUsers";
 import styles from "../styles/Home.module.scss"
-export default function userPage() {
+import { useState, useEffect } from 'react';
 
+export default function userPage() {
+  
+  const [user, setUser] = useState([]);
   
   const history = useRouter();
 
-  const handleLogout = () => {
-    localStorage.removeItem('token'); //Remove the token from local storage
-    history.push('/logoutPage'); // Redirect to the start page
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      history.push('/login'); // Redirect to the login page if not logged in
+      return;
+    }
+
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/auth/getUserInfo", {
+          headers: {
+            method: 'GET',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const userData = await response.json();
+        setUser(userData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUser();
+  }, [history]);
+
+  const handleUpdateUser = async () => {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch('/api/v1/auth/update', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          // the updated user data
+        }),
+      });
+      const data = await response.json();
+      console.log(data); // log the response from the backend
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleDeleteUser = async ()=> {
@@ -32,49 +75,28 @@ export default function userPage() {
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem('token'); //Remove the token from local storage
+    history.push('/logoutPage'); // Redirect to the start page
+  };
 
-    const handleUpdateUser = async () => {
-      const token = localStorage.getItem('token');
-      try {
-        const response = await fetch('/api/v1/auth/update', {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            // the updated user data
-          }),
-        });
-        const data = await response.json();
-        console.log(data); // log the response from the backend
-        console.log(user);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-    <h1>UserPage</h1>
-
-    <div className={styles.logoutContainer}>
-    <Button onClick={handleLogout}>Logout</Button>
-    </div>
-   
-
-    <div className={styles.updateContainer}>
-    <Button onClick={handleUpdateUser}>Update User</Button>
-    </div>
-    
-
-    <div className={styles.deleteContainer}>
-      
+      <main className={styles.main}>
+      <h1>{user.firstname}'s Profile Page</h1>
+      <form action="">
+      <p key={user.id}>Email: {user.email}</p>
+      <p key={user.id}>Firstname: {user.firstname}</p>
+      <p key={user.id}>Lastname: {user.lastname}</p>
+      </form>
+      <Button onClick={handleLogout}>Logout</Button>
+      <Button onClick={handleUpdateUser}>Update User</Button>
       <Button onClick={handleDeleteUser}>Delete User</Button>
+      </main>
     </div>
-
-    <CheckUsers/>
-    
-    </div>
-  )
+  );
 }
